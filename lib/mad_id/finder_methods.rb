93530851337c832bc3b09e49a1cb866d84f6f584
Michael Bumann <michael@railslove.com>
2014-10-28 13:09:20 +0100

    add locate and locate! methods to MadID to make raising errors optional
    this changes the behaviour of locate and adds a locate! method.
    the locate! method raises a KeyError error if no class is found in the
    registry and an ActiveRecord::RecordNotFoundError if no record is found
    
    the locate method simply ignores those errors and returns nil. This is
    helpful when you expect the search for strange IDs but do not care if
    nothing is found.
    
    NOTE: this changes the current API

--- lib/mad_id/finder_methods.rb
+++ lib/mad_id/finder_methods.rb
@@ -3,5 +3,8 @@ module MadID::FinderMethods
   def find_by_mad_id!(id)
     find_by!(identifier: id.to_s.downcase)
   end
+  def find_by_mad_id(id)
+    find_by(identifier: id.to_s.downcase)
+  end
 
 end
